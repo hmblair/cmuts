09e566ec2575b13fb76ebc63eea6af88347a7e31
Hamish B <hmblair@stanford.edu>
2024-09-27 14:07:55 -0700

    Fist commit.

---
+++ cmuts.h
@@ -0,0 +1,10 @@
+#define VERSION "0.1.0"
+// The name of the datasets in the HDF5 file
+#define MUTATION_DS "mutations"
+#define INSERTION_DS "insertions"
+// The number of dimensions of the respective
+// datasets
+#define NUM_MUTDIMS 4
+#define NUM_INSDIMS 3
+// The default out file
+#define DEFAULT_OUT "count.h5"
