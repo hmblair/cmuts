82dd234e9be20673d69d79616338fdd3e123d775
Hamish B <hmblair@stanford.edu>
2025-09-12 01:22:14 -0700

    Added conditional, cooccurrence

--- src/scripts/cov.py
+++ src/scripts/cov.py
@@ -1,21 +1,57 @@
 import numpy as np
 
 
-def covariance(joint: np.ndarray) -> np.ndarray:
+def probability(counts: np.ndarray) -> np.ndarray:
 
-    joint = joint / joint.sum((-2, -1))[..., None, None]
+    prob = np.zeros_like(counts)
+    prob[..., 0, 0] = 1
 
-    ex = (joint[..., 1, 1] + joint[..., 1, 0]).mean(1)
-    ey = (joint[..., 1, 1] + joint[..., 0, 1]).mean(0)
-    exy = joint[..., 1, 1]
+    counts_s = counts.sum((-1, -2))[..., None, None]
+    return np.divide(
+        counts,
+        counts_s,
+        where=(counts_s > 0),
+        out=prob,
+    )
+
+
+def conditional(counts: np.ndarray) -> np.ndarray:
+
+    prob = probability(counts)
+    joint = prob[..., 1, 1]
+    marginal = prob[..., 1, 1] + prob[..., 1, 0]
+
+    cond = np.divide(
+        joint,
+        marginal,
+        where=(marginal > 0),
+        out=np.zeros_like(joint)
+    )
+    return cond
+
+
+def covariance(counts: np.ndarray) -> np.ndarray:
+
+    prob = probability(counts)
+    prob = prob / prob.sum((-2, -1))[..., None, None]
+
+    ex = (prob[..., 1, 1] + prob[..., 1, 0]).mean(1)
+    ey = (prob[..., 1, 1] + prob[..., 0, 1]).mean(0)
+    exy = prob[..., 1, 1]
 
     return exy - ex[None, :] * ey[:, None]
 
 
-def correlation(joint: np.ndarray) -> np.ndarray:
+def correlation(counts: np.ndarray) -> np.ndarray:
 
-    cov = covariance(joint)
+    cov = covariance(counts)
     std = np.sqrt(np.diag(cov))
 
     cov = cov / (std[:, None] * std[None, :])
     return np.nan_to_num(cov, 0.0)
+
+
+def cooccurrence(counts: np.ndarray) -> np.ndarray:
+
+    counts_s = counts.sum((-2, -1))
+    return counts_s / counts_s.sum(1)
