50eccedfa51c98cfeb0a571625fd0734bf193a7e
Hamish B <hmblair@stanford.edu>
2025-11-03 12:13:12 -0800

    Refactor

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
+DIR = Path(__file__).parent
+NAME = DIR.name
+VERSION = (DIR / "bin" / "VERSION").read_text().strip()
+AUTHOR = "Hamish M. Blair"
+EMAIL = "hmblair@stanford.edu"
+URL = "https://github.com/hmblair/cmuts"
+
 setup(
-    name="cmuts",
-    version="0.1.0",
-    author="Hamish M. Blair",
-    author_email="hmblair@stanford.edu",
-    url="https://github.com/hmblair/cmuts",
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
     package_dir={"": "src/python"},
     packages=find_packages(where="src/python"),
 )
