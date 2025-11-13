2571fc0bcf5419d0457cb1cd96785c72f7763504
Hamish B <hmblair@stanford.edu>
2025-11-13 13:07:10 -0800

    Added license

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ VERSION = (DIR / "VERSION").read_text().strip()
 AUTHOR = "Hamish M. Blair"
 EMAIL = "hmblair@stanford.edu"
 URL = "https://github.com/hmblair/cmuts"
+LICENSE = "MIT"
 
 setup(
     name=NAME,
@@ -14,6 +15,7 @@ setup(
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
+    license=LICENSE,
     package_dir={"": "src/python"},
     packages=find_packages(where="src/python"),
 )
